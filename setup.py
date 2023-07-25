5fdecab691bc447aaaf99b8bfb8e1163e53d7b02
ayudmin <vikrammehra244@gmail.com>
2023-07-25 13:27:57 +0200

    info: patch v0.2.4

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
 name = "dj_maintenance",
 version = "0.2.4",
-long_description="dj_maintenance is a Django app to conduct sheduled dj-dj_maintenance on your web application. Detailed documentation is in the 'docs' directory."
+long_description="dj_maintenance is a Django app to conduct sheduled dj_maintenance on your web application. Detailed documentation is in the 'docs' directory."
 )
