66b00bc703141f5c93a51a6cf973a7b448464b1b
ayudmin <vikrammehra244@gmail.com>
2023-07-25 10:24:02 +0200

    info: rename project and info

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
 
 setup(
-    long_description="foo bar baz"
+    long_description="dj-maintenance is a Django app to conduct sheduled maintenance on your web application. Detailed documentation is in the 'docs' directory."
 )
