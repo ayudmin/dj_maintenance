181c8f822f0beb8a5520d79d92f5d97fb2b0ed08
ayudmin <vikrammehra244@gmail.com>
2023-07-26 11:19:27 +0200

    info: remove context declaration

--- dj_maintenance/views.py
+++ dj_maintenance/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def index(request, resource=None):
-    return render(request, 'dj_maintenance/index.html', conttext)
+    return render(request, 'dj_maintenance/index.html')
