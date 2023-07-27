0faf93ec5f9071d629bcd44749c26b2d0619eb2f
ayudmin <vikrammehra244@gmail.com>
2023-07-27 18:03:28 +0200

    info: fix errors

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 
 setup(
-    long_description=" dj_maintenance is a Django app to conduct sheduled maintenance on your web application. "
-                     "Detailed documentation is in the 'docs' directory."
+    long_description=" dj_maintenance is a Django app to conduct sheduled maintenance on your web application."
 )
